f26cde3e28049d45f68d6c3d524501bdea5ff2b1
Moayadooh <moayadsalim97@gmail.com>
2025-05-15 11:11:58 +0400

    create header

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-header',
+    //template: '<h1>Hello World</h1>',
+    //standalone: true, //default value is true in Angular v19 and higher
+    templateUrl: './header.component.html',
+    styleUrl: './header.component.css'
+})
+export class HeaderComponent {}
